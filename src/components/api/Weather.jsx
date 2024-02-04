25a7e814c4a4265375b270b1689bc105bc0702e9
minju-sin <sinminju04@gmail.com>
2024-02-04 13:05:40 +0900

    현재 날짜 함수 생성

--- src/components/api/Weather.jsx
+++ src/components/api/Weather.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
+import { getCurrentDate } from './dateUtils';
 
 function Weather() {
     const [responseData, setResponseData] = useState(null); // 요청 결과
@@ -9,12 +10,8 @@ function Weather() {
     const URL = process.env.REACT_APP_URL; // 요청 url 
     const apiKey = process.env.REACT_APP_API_KEY; // service Key 
 
-    // 현재 날짜를 'YYYYMMDD' 형식으로 얻기 20240202
-    const currentDate = new Date();
-    const year = currentDate.getFullYear();
-    const month = String(currentDate.getMonth() + 1).padStart(2, '0'); // 월은 0부터 시작하므로 1을 더하고 두 자리로 패딩
-    const day = String(currentDate.getDate()).padStart(2, '0'); // 일을 두 자리로 패딩
-    const baseDate = `${year}${month}${day}`;
+    // 현재 날짜를 'YYYYMMDD' 형식으로 얻기 
+    const baseDate = getCurrentDate();
 
     // 현재 시간 얻기 
     const baseTime = '0500';
--- src/components/api/dateUtils.js
+++ src/components/api/dateUtils.js
@@ -1,10 +1,9 @@
 // dateUtils.js
-
-export const getFormattedDate = () => {
-    const currentDate = new Date();
+// 현재 날짜를 'YYYYMMDD' 형식으로 얻기
+export function getCurrentDate() {
+  const currentDate = new Date();
     const year = currentDate.getFullYear();
-    const month = String(currentDate.getMonth() + 1).padStart(2, '0');
-    const day = String(currentDate.getDate()).padStart(2, '0');
-    return `${year}${month}${day}`;
-  };
-  
+    const month = String(currentDate.getMonth() + 1).padStart(2, '0'); // 월은 0부터 시작하므로 1을 더하고 두 자리로 패딩
+    const day = String(currentDate.getDate()).padStart(2, '0'); // 일을 두 자리로 패딩
+  return `${year}${month}${day}`;
+}
